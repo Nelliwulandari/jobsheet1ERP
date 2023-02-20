11011f38dfbba665814f2b10a1a04fce0df5c910
Nelliwulandari <nelliwlnd30@gmail.com>
2023-02-20 15:18:47 +0700

    upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+"name"          : "New Addons",
+"version"       : "11.0",
+"author"        : "Nelli Wulandari",
+"website"       : "https://www.kaggle.com/",
+"category"      : "New Module",
+"Summary"       : "Ini adalah addons baru",
+"description"   : """
+    Latihan membuat addons baru di odoo 1
+""",
+"depends"       : [
+    "base",
+],
+"data"          : [
+
+],
+"demo"          : [],
+"test"          : [],
+"image"         : [],
+"qweb"          : [],
+"css"           : [],
+"application"   : True,
+"intallable"    : True,
+"auto_install"  : False,
+}
